155cb8da9c7bc35745febad1f7ff32e6bb96041b
ByteBandit-100 <ghsmohit2004@gmail.com>
2026-06-13 17:26:10 +0530

    change font poppins

--- contactbookApp/main.py
+++ contactbookApp/main.py
@@ -1,15 +1,15 @@
 import tkinter as tk
 
 def welcomeWindow(rt):
-    # tk.Label(text="Welcome to ContactBook App", justify="center").grid(row=0, column=0)
-
-    label = tk.Label(rt, text="Contact Book", height=10, width=100)
-    label.pack()
-    label.pack(expand=True, side="top")
+    label = tk.Label(rt, text="Welcome in Contact Book",image=icon,
+    compound="left", font=("poppins", 15) )
+    label.pack( side="top")
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("Contact Book")
-    root.geometry("600x400")  #window size
+    icon = tk.PhotoImage(file="./images/contact1.png")
+    root.geometry("650x500")  #window size
+    root.iconphoto(False,icon)
     welcomeWindow(root)
     root.mainloop()
